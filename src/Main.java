34693faf47ed80e496e62ccdb03f047e264b8dac
Artyom Markovich <marko.artem@gmail.com>
2021-11-01 09:00:21 +0300

    hw_#1

--- src/Main.java
+++ src/Main.java
@@ -18,20 +18,54 @@ public class Main {
         String task = new BufferedReader(new InputStreamReader(System.in)).readLine();//считываем текст с клавиатуры
 
         if (task.equals("бага")) {
-            System.out.println("Это не бага, это фича");
-        }
-
-        if (task.equals("фича")) {
             System.out.println("Это не фича, это бага");
         }
 
+        if (task.equals("фича")) {
+            System.out.println("Это не бага, это фича");
+        }
+        else {
+            System.out.println("Это не фича и не бага");
+        }
         /**
          * Задание на циклические операторы
          */
+        System.out.println("Это бага или фича? Введите слово: ");//выводим в консоль текст
+
+          String mask = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        switch(mask) {
+            case "бага" : System.out.println("да, это бага");
+                 break;
+            case "фича" : System.out.println("да, это фича");
+                 break;
+            default:  System.out.println("Это не бага и не фича");
+                 break;
+        }
+
 
         //1. Написать цикл while, который будет выводить числа от 1 до 10
+        int v = 1;
+        while (v < 11) {
+            System.out.println(" " +v);
+            v++;
+        }
         //2. Написать цикл while, который будет выводить числа от 10 до 1
+        int t = 10;
+        while (t >= 1) {
+            System.out.println(t);
+            t--;
+        }
         //3. Написать цикл while, который будет выводить только четные числа от 1 до 10, т.е. 2,4,6,8
+        int c = 2;
+        while (c < 10) {
+            System.out.println(" " + c);
+            c += 2;
+        }
         //4. Написать цикл while, который будет выводить только нечетные числа от 1 до 10, т.е. 1,3,5,7,9
+        int r = 1;
+        while (r < 10) {
+            System.out.println(" " + r);
+            r += 2;
+        }
     }
 }
