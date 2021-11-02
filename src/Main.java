cb36a17d93d7c2a4afa0a92e83276497132dda8a
LiaBash <39912081jb@gmail.com>
2021-11-02 14:56:49 +0300

    Домашнее задание от 25.10.2021

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,33 @@ public class Main {
             System.out.println("Это не бага, это фича");
         }
 
-        if (task.equals("фича")) {
+        else if (task.equals("фича")) {
             System.out.println("Это не фича, это бага");
         }
+        else {
+            System.out.println("Это не фича, и это не бага");
+        }
+
+        System.out.println();
+
+        System.out.println("Это бага или фича? Введите слово: ");
+
+        String task2 = new BufferedReader(new InputStreamReader(System.in)).readLine();
+
+        switch (task2){
+            case "бага":
+                System.out.println("Это не бага, это фича");
+                break;
+            case "фича":
+                System.out.println("Это не фича, это бага");
+                break;
+            default:
+                System.out.println("Это не фича, и это не бага");
+
+        }
+
+        System.out.println();
+
 
         /**
          * Задание на циклические операторы.
@@ -32,5 +56,40 @@ public class Main {
         //2. Написать цикл while, который будет выводить числа от 10 до 1
         //3. Написать цикл while, который будет выводить только четные числа от 1 до 10, т.е. 2,4,6,8
         //4. Написать цикл while, который будет выводить только нечетные числа от 1 до 10, т.е. 1,3,5,7,9
+
+        int a = 1;
+        while (a != 11) {
+            System.out.println(a);
+            a++;
+        }
+
+        System.out.println();
+
+        int b = 10;
+        while (b != 0) {
+            System.out.println(b);
+            b--;
+        }
+
+        System.out.println();
+
+        int c = 2;
+        while (c != 10) {
+            System.out.println(c);
+            c = c+2;
+        }
+
+
+        System.out.println();
+
+        int d = 1;
+        while (d != 11) {
+            System.out.println(d);
+            d = d+2;
+        }
+
+
+
+
     }
 }
